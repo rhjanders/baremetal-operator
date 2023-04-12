e95dfdbcfe1e247c49fb514b4e1fdfae0de36de7
Zane Bitter <zbitter@redhat.com>
2023-04-12 16:59:15 +1200

    Don't munge timestamp in structured logs
    Structured logging tools expect a POSIX timestamp. Only change the
    timestamp formatting in development log mode.
    
    (cherry picked from commit 0a887a16fb26dd26a2b372953ec8d860a3a251b5)
    Signed-off-by: Honza Pokorny <honza@redhat.com>

--- main.go
+++ main.go
@@ -117,15 +117,14 @@ func main() {
 	// NOTE (honza): Downstream only: disabling webhooks until CBO supports them
 	flag.IntVar(&webhookPort, "webhook-port", 0,
 		"Webhook Server port (set to 0 to disable)")
-	opts := zap.Options{
-		Development: devLogging,
-		TimeEncoder: zapcore.ISO8601TimeEncoder,
-	}
-	opts.BindFlags(flag.CommandLine)
-
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	logOpts := zap.Options{}
+	if devLogging {
+		logOpts.Development = true
+		logOpts.TimeEncoder = zapcore.ISO8601TimeEncoder
+	}
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&logOpts)))
 
 	printVersion()
 
